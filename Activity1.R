9d63bfb6ca3a0bdbc31dce25c57e143c543c4102
isabeldrexlerbooth <idrexlerbooth@colgate.edu>
2025-09-04 11:09:32 -0400

    Finishing up activity 1

--- Activity1.R
+++ Activity1.R
@@ -1,1 +1,17 @@
 print("hello world")
+# Warmup equations
+6^6
+234+8
+15-23
+a <- 6234
+a*5
+
+#Setting up variables
+slice <- 1:5
+two <- 2
+notThree <- 4
+#Playing with putting variables into equations
+slice + two
+notThree - two
+4+two
+notThree/1
