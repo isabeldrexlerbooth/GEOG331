66607b46773aae5858b0754cd6cee186a0c88fd1
isabeldrexlerbooth <idrexlerbooth@colgate.edu>
2025-10-28 11:11:27 -0400

    Started Activity 5

---
+++ Activity5.R
@@ -0,0 +1,45 @@
+# Isabel Drexler Booth
+# Homework 5
+
+#SETTING UP DATA AND VARIABLES
+library(lubridate)
+datH <- read.csv("Z:\\idrexlerbooth\\data\\hw5_data\\stream_flow_data.csv",
+                 na.strings = c("Eqp"))
+  
+datP <- read.csv("Z:\\idrexlerbooth\\data\\hw5_data\\2049867.csv")                            
+  
+datD <- datH[datH$discharge.flag == "A",]
+#### define time for streamflow #####
+#convert date and time
+datesD <- as.Date(datD$date, "%m/%d/%Y")
+#get day of year
+datD$doy <- yday(datesD)
+#calculate year
+datD$year <- year(datesD)
+#define time
+timesD <- hm(datD$time)
+
+#### define time for precipitation #####    
+dateP <- ymd_hm(datP$DATE)
+#get day of year
+datP$doy <- yday(dateP)
+#get year 
+datP$year <- year(dateP)
+
+#### get decimal formats #####
+#convert time from a string to a more usable format
+#with a decimal hour
+datD$hour <- hour(timesD ) + (minute(timesD )/60)
+#get full decimal time
+datD$decDay <- datD$doy + (datD$hour/24)
+#calculate a decimal year, but account for leap year
+datD$decYear <- ifelse(leap_year(datD$year),datD$year + (datD$decDay/366),
+                       datD$year + (datD$decDay/365))
+#calculate times for datP                       
+datP$hour <- hour(dateP ) + (minute(dateP )/60)
+#get full decimal time
+datP$decDay <- datP$doy + (datP$hour/24)
+#calculate a decimal year, but account for leap year
+datP$decYear <- ifelse(leap_year(datP$year),datP$year + (datP$decDay/366),
+                       datP$year + (datP$decDay/365)) 
+
