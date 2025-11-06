eac19dd8baf7264fb71b7d39aa825a5df3ed7f84
isabeldrexlerbooth <idrexlerbooth@colgate.edu>
2025-11-06 11:18:24 -0500

    Read in first two files of dataset, need to make a boxplot figure

---
+++ bisonProject.R
@@ -0,0 +1,7 @@
+#Isabel Drexler Booth
+#read in oak sapling growth data
+datGrowth <- read.delim("Z:\\idrexlerbooth\\data\\project_data\\knb-lter-cdr.703.1\\e321_Oak sapling growth.txt",
+                      header = TRUE)
+#read in carbon and nitrogen percent
+datPercent <- read.delim("Z:\\idrexlerbooth\\data\\project_data\\knb-lter-cdr.703.1\\e321_Soil Carbon and Nitrogen.txt",
+                        header = TRUE)
